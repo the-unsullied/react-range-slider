ae111074d40c7a2f0db0e6ac67793d9cabcc52b1
Matt Goo <mattgoo@gmail.com>
2016-04-21 14:50:32 -0700

    created react-range-slider

---
+++ scripts/build-dist.js
@@ -0,0 +1,16 @@
+var sass = require('node-sass');
+var fs = require('fs');
+
+sass.render({
+  file: './src/main.scss'
+}, function(error, result) { // node-style callback from v3.0.0 onwards
+  if(!error){
+    fs.writeFile('./dist/RangerSlider.css', result.css, function(err){
+      if(!err){
+        console.log('css file written to disk');
+      }
+    });
+  } else {
+    console.log(error);
+  }
+});
